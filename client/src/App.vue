10de6027e5bd58ee60ea5db2cdc8a24102723300
Paulo Morandi <paulomorandi@mobiltec.com.br>
2022-07-30 16:09:18 -0300

    Fixing vue.js to work with docker.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,110 +1,99 @@
-<script>
+<script setup lang="ts">
 import axios from "axios";
-export default {
-  data() {
-    return {
-      board: {
-        name: "Project A",
-        columns: [
-          {
-            name: "Todo",
-            cards: [
-              { title: "A", estimative: 3 },
-              { title: "B", estimative: 3 },
-              { title: "C", estimative: 3 },
-            ],
-          },
-          { name: "Doing", cards: [] },
-          { name: "Done", cards: [] },
-        ],
-      },
-      columnName: "",
-      cardTitle: ""
-    };
-  },
-  async mounted() {
-    console.log("chamado quando o módulo é construído");
-    const response = await axios({
-      url: "http://localhost:3001/boards/1",
-      method: "get"
-    });
-    this.board = response.data;
-  },
-  methods: {
-    addColumn(columnName) {
-      this.board.columns.push({name: columnName, cards: []});
-    },
-    addCard(column, cardTitle) {
-      column.cards.push({title: cardTitle, estimative: 3});
-    },
-    increaseEstimative(card) {
-      card.estimative++;
-    }
-  },
-  computed:{
-    boardEstimative() {
-      return this.board.columns.reduce((total, column)=>{
-        total += column.estimative;
-        return total;
-      },0);
-    }
-  }
-};
+import { computed, onMounted, reactive, ref } from "vue";
+
+let data: any = reactive({});
+let columnName: any = ref("");
+let cardTitle = "";
+
+function addColumn (columnName: string) {
+	data.board.columns.push({ name: columnName, cards: [] });
+}
+function addCard (column: any, cardTitle: string) {
+	column.cards.push({ title: cardTitle, estimative: 3 });
+	column.estimative += 3;
+}
+function increaseEstimative (card: any) {
+	card.estimative++;
+}
+const boardEstimative = computed(() => {
+	return data.board.columns.reduce((total: number, column: any) => {
+		total += column.cards.reduce((total: number, card: any) => {
+			total += card.estimative;
+			return total;
+		}, 0);
+		return total;
+	}, 0);
+});
+
+onMounted(async () => {
+	const response = await axios({
+		url: "http://localhost:3000/boards/7",
+		method: "get"
+	});
+	data.board = response.data;
+});
 </script>
 
 <template>
-  <h3>{{ board.name }} {{ boardEstimative() }}</h3>
-  <div class="columns">
-    <div class="column" v-for="column in board.columns">
-      <h3>{{ column.name }} {{ column.estimative }}</h3>
-      <div class="card" v-for="card in column.card">
-        {{ card.title }} {{ card.estimative }}
-        <br />
-        <button @click="increaseEstimative(card)">+</button><button>-</button>
-      </div>
-      <div class="new-card">
-        <input type="text" v-model="cardTitle" />
-        <button v-on:click="addCard(cardTitle)">Add</button>
-      </div>
-    </div>
-    <div class="column new-column">
-      <input type="text" v-model="columnName" />
-      <button v-on:click="addColumn(columnName)">Add</button>
-    </div>
-  </div>
+	<div v-if="data.board">
+		<h3>{{ data.board.name }} {{ boardEstimative }}</h3>
+		<div class="columns">
+			<div class="column" v-for="column in data.board.columns">
+				<h3>{{ column.name }} {{ column.estimative }}</h3>
+				<div class="card" v-for="card in column.cards">
+					{{ card.title }} {{ card.estimative }}
+					<br/>
+					<button @click="increaseEstimative(card)">+</button><button>-</button>
+				</div>
+				<div class="card new-card">
+					<input type="text" v-model="cardTitle" />
+					<button v-on:click="addCard(column, cardTitle)">Add</button>	
+				</div>
+			</div>
+			<div class="column new-column">
+				{{ columnName }}
+				<input type="text" v-model="columnName" />
+				<button v-on:click="addColumn(columnName)">Add</button>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style scoped>
 .columns {
-  display: flex;
-  flex-direction: row;
+	display: flex;
+	flex-direction: row;
 }
 .column {
-  width: 200px;
-  text-align: center;
-  background-color: #ccc;
-  margin-right: 5px;
-  padding: 10px;
-  border: 1px solid #000;
-}
-.new-column {
-  background-color: #eee;
-  border: 1px dashed #ccc;
-}
-.card {
-  text-align: center;
-  width: 100%;
-  height: 80px;
-  background-color: #f3e779;
-  border: 1px solid #000;
-  margin-bottom: 10px;
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
+	width: 200px;
+	text-align: center;
+	background-color: #CCC;
+	margin-right: 5px;
+	padding: 10px;
+	border: 1px solid #000;
 }
 
+.new-column {
+	background-color: #EEE;
+	border: 1px dashed #CCC;
+	display: block;
+}
+
+.card {
+	text-align: center;
+	width: 100%;
+	height: 80px;
+	background-color: #F3E779;
+	border: 1px solid #000;
+	margin-bottom: 10px;
+	display: flex;
+	align-items: center;
+	justify-content: space-around;
+}
 .new-card {
-  background-color: #eee;
-  border: 1px dashed #ccc;
+	background-color: rgb(229, 231, 169);
+	border: 1px dashed #CCC;
+	display: block;
 }
 </style>

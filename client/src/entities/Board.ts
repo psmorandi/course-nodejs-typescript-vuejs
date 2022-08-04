e01be6e8f676cca758626e5ef581bc0399784c44
Paulo Morandi <paulomorandi@mobiltec.com.br>
2022-08-04 19:19:21 -0300

    Installing and configuring vitest.

---
+++ client/src/entities/Board.ts
@@ -0,0 +1,10 @@
+export default class Board {
+  columns: any[];
+  constructor(readonly name: string) {
+    this.columns = [];
+  }
+
+  getEstimative() {
+    return 0;
+  }
+}
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,18 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+  },
   plugins: [vue()],
   server: {
     host: true,
     port: 5173,
     watch: {
-      usePolling: true
-    }
-  }
+      usePolling: true,
+    },
+  },
 });

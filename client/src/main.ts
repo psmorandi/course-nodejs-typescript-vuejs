4f964338b2e0f4e81f276d5d2bd6f0a3825c192e
Paulo Morandi <paulomorandi@mobiltec.com.br>
2022-08-06 16:26:01 -0300

    Fixed api configuration.

--- client/src/main.ts
+++ client/src/main.ts
@@ -5,7 +5,7 @@ import BoardServiceHttp from "./services/BoardServiceHttp";
 
 const app = createApp(App);
 const httpClient = new AxiosAdapter();
-const baseUrl = "http://localhost:3001";
+const baseUrl = "http://localhost:3000";
 const boardServiceHttp = new BoardServiceHttp(httpClient, baseUrl);
 app.provide("boardService", boardServiceHttp);
 app.mount("#app");
